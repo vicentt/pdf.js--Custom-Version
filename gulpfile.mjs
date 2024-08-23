f437d9d61871a082c29e6fe97344b64a73fe70a4
vmorenododepecho <vmorenododepecho@sgcristaleria.onmicrosoft.com>
2024-08-23 11:45:30 +0200

    Added custom ToolBar file and references to it in viewer,  and gulpfile.

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1033,6 +1033,10 @@ function buildGeneric(defines, dir) {
     gulp
       .src("web/compressed.tracemonkey-pldi-09.pdf", { encoding: false })
       .pipe(gulp.dest(dir + "web")),
+      
+    gulp
+      .src("web/customToolbar.js", { encoding: false })
+      .pipe(gulp.dest(dir + "web")),
   ]);
 }
 
